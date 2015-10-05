68dbcec027bb29b73c01c7cd02b802b88ef1d751
Julian Blacher <julian.blacher@gmail.com>
2015-10-05 17:40:48 -0300

    adding first steps of script.js to hide/show divs

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 <script src="https://togetherjs.com/togetherjs-min.js"></script>
+
+var crearDivs = function(){
+	for (i=1;i<10;i++){
+        console.log(i);
+        var d = document.createElement("div");
+	};
+};
+
+$(document).ready(function() { 
+    $('#container').mouseenter(function() { 
+        $('div').hide();
+        $('div').fadeTo('fast','0.8');
+    });
+    $('#container').mouseleave(function() { 
+        $('div').show();
+        $('div').fadeTo('fast','1');
+    });
+    $('#main').draggable();
+    $('#main').resizable();
+});
+
+
+
+
+crearDivs();
