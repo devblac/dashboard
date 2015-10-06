0d652d0b9de2fd823b0f059a2d3d442850660c8e
Julian Blacher <julian.blacher@gmail.com>
2015-10-06 12:53:14 -0300

    Addin dragging and resizing of each div with the ids

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-<script src="https://togetherjs.com/togetherjs-min.js"></script>
-
+/*
 var crearDivs = function(){
 	for (i=1;i<10;i++){
         console.log(i);
         
-        var divElement = document.createElement("div");        // Create a <button> element
-		var divTextElement = document.createTextNode(i);       // Create a text node
-		divElement.appendChild(divTextElement);                                // Append the text to <button>
+        var divElement = document.createElement("div");        
+		var divTextElement = document.createTextNode(i);       
+		divElement.appendChild(divTextElement);                
 		document.body.appendChild(divElement);
-
+		
+        
 	};
 };
-
-$(document).ready(function() { 
-    $('#container').mouseenter(function() { 
-        $('div').hide();
-        $('div').fadeTo('fast','0.8');
+*/
+$(document).ready(function() {
+    /*
+    var divsArray = [1,2,3,4,5,6,7,8,9]
+    for (i=0;i<divsArray.length;i++){
+        $('div #i').draggable();
+    	$('div #i').resizable();
+    };	*/	
+    	
+    $('#1, #2, #3, #4, #5, #6, #7, #8, #9').draggable(function(){
+        
     });
+    $('#1, #2, #3, #4, #5, #6, #7, #8, #9').resizable(function(){
+        
+    });
+
+    
+    $('div').mouseenter(function() { 
+        $('div #').fadeTo('fast','0.8');
+    });
+    
     $('#container').mouseleave(function() { 
         $('div').show();
         $('div').fadeTo('fast','1');
     });
-    $('#main').draggable();
-    $('#main').resizable();
+    
 });
 
-
+/*
 crearDivs();
+*/
