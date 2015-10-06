d4c8811d0f424b302ba7ef936e2742d651f43cbd
Julian Blacher <julian.blacher@gmail.com>
2015-10-06 17:32:53 -0300

    Optimized code, added a for and solved fadeIn/fadeOut

--- scripts/script.js
+++ scripts/script.js
@@ -16,27 +16,31 @@ var crearDivs = function(){
 */
 $(document).ready(function() {
     /*
-    var divsArray = [1,2,3,4,5,6,7,8,9]
+    var divsArray = [1,2,3,4,5,6,7,8,9];
     for (i=0;i<divsArray.length;i++){
         $('div #i').draggable();
     	$('div #i').resizable();
     };	*/	
-    	
+    	/*
     $('#1, #2, #3, #4, #5, #6, #7, #8, #9').draggable();
     $('#1, #2, #3, #4, #5, #6, #7, #8, #9').resizable();
+        */
 
+    for (i=0;i<10;i++){
+        $('#'+i+'').draggable();
+        $('#'+i+'').resizable();
+    };
     
-    $('div').mouseenter(function() { 
-        $('div #').fadeTo('fast','0.8');
-    });
-    
-    $('#container').mouseleave(function() { 
-        $('div').show();
-        $('div').fadeTo('fast','1');
-    });
-    
+        $('.box').mouseenter(function() { 
+            $(this).fadeTo('fast','0.4');
+        });
+        
+        $('.box').mouseleave(function() { 
+            $(this).fadeTo('fast','1');
+        });
 });
 
 /*
+'#'+this.id
 crearDivs();
 */
