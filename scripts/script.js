89aabaf56a6e9f577d44be4ac2bccc4b7f47ce99
Julian Blacher <julian.blacher@gmail.com>
2015-10-08 19:00:50 -0300

    Solved the problem with mouseleave function, which turns false the hide() function to the li element.

--- scripts/script.js
+++ scripts/script.js
@@ -1,79 +1,61 @@
-
-
-/* Intento fallido para crear los divs desde JS
-var crearDivs = function(){
-	for (i=1;i<10;i++){
-        console.log(i);
-        
-        var divElement = document.createElement("div");        
-		var divTextElement = document.createTextNode(i);       
-		divElement.appendChild(divTextElement);                
-		document.body.appendChild(divElement);
-		
-        
-	};
-};
-*/
 $(document).ready(function() {
     
     /* Intento cero de mover y resizear divs. 
-
     var divsArray = [1,2,3,4,5,6,7,8,9];
     for (i=0;i<divsArray.length;i++){
         $('div #i').draggable();
     	$('div #i').resizable();
-    };	*/	
+    };  */	
     
-
-    	/* Primer intento (satisfactorio) de mover y resizear divs:
+    /* Primer intento (satisfactorio) de mover y resizear divs:
     $('#1, #2, #3, #4, #5, #6, #7, #8, #9').draggable();
     $('#1, #2, #3, #4, #5, #6, #7, #8, #9').resizable();
-        */
+    */
     
 
-        /* Método optimizado para mover y resizear los divs:
+    /* Método optimizado para mover y resizear los divs:
     for (i=0;i<10;i++){
         $('#'+i+'').draggable();
         $('#'+i+'').resizable();  
     };  */
-    /*
+    /*  Que se puedan mover uno encima de otro, de manera caótica:
     $('li').mouseenter(function() { 
         $(this).draggable();
     });
     */
+    // Modificar tamaño libremente:
     $('li').mouseenter(function() { 
         $(this).resizable();
     });
+    
     /* QUITAR COMMENT
     $('li').mouseenter(function() { 
         $(this).fadeTo('fast','0.6');
     });
     */
+
     /* $('#'+i+'').resizable(); */
 
-
+    // Ocultar boxes:
     $('div.box').click(function(){
         $(this).hide();
         $(this).parent('li').hide();
     });
 
-    /*
+
+    // Hacer fades a los boxes al pasar, el fadeOut verifica si es visible ó no:
     $('li').mouseenter(function() { 
         $(this).fadeTo('fast','0.6');
     });
-        
+    
     $('li').mouseleave(function() { 
-        $(this).fadeTo('fast','1');
+        if($(this).is(":visible")){
+            $(this).fadeTo('fast','1');
+        }
     });
-    */
+
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
     $( "#sortable" ).selectable();
 });
 
-
-
-/*
-'#'+this.id
-crearDivs();
-*/
